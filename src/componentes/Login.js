2a8afee57b1da361dc4bfdc1ce9cbea4668bcb3c
Danilo Paixão <danilopaixao@Admins-Mac-mini.local>
2018-07-30 17:57:07 -0300

    novas imagens do app de denuncia da saude

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -39,11 +39,14 @@ export default class Login extends Component {
     render(){
         return (
             <div className="login-box">
-                <h1 className="header-logo">Instalura</h1>
+                <h1 className="header-logo">LogoApp</h1>
                 <span>{this.state.msg}</span>
                 <form onSubmit={this.envia.bind(this)}>
-                    <input type="text" ref={(input) => this.login = input}/>
-                    <input type="password" ref={(input) => this.senha = input}/>
+                    <input type="text" ref={(input) => this.login = input} value="rafael" /> 
+                    {
+                        //dados para teste-->
+                    }
+                    <input type="password" ref={(input) => this.senha = input} value="123456" />
                     <input type="submit" value="login"/>
                 </form>
             </div>
